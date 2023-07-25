e6ca6b19fa242cfb6cdb1a4285d6c63e6713f9d0
sjfox <sjfcodes@gmail.com>
2023-07-24 22:23:51 -0700

    its alive!!!!

--- src/components/KittyHeader/index.js
+++ src/components/KittyHeader/index.js
@@ -1,28 +1,27 @@
-import React, { Suspense, useState } from "react";
-import { Canvas, useThree } from "react-three-fiber";
-import './KittyHeader.css';
+import React, { Suspense } from "react";
+import { Canvas } from "react-three-fiber";
+import "./KittyHeader.css";
 import Kitty from "../3D/Kitty";
 import ManageLights from "../3D/ManageLights";
-// import { OrbitControls } from "@react-three/drei";
+import { OrbitControls } from "@react-three/drei";
 
-function KittyHeader( { isPlaying } ) {
-
+function KittyHeader({ isPlaying }) {
   return (
     <>
       <Canvas
         colorManagement
         shadowMap
-        // camera={{ position: [0, 2, 16], fov: 40 }}>
-        camera={{ position: [0, 2, 20], fov: 40 }}>
+        camera={{ position: [0, 2, 20], fov: 40 }}
+      >
         <ambientLight intensity={0.7} />
         <ManageLights isPlaying={isPlaying} />
-        {/* <OrbitControls /> */}
-        <Suspense fallback={null} >
+        <OrbitControls />
+        <Suspense fallback={null}>
           <Kitty isPlaying={isPlaying} position={[0, -2, 0]} />
         </Suspense>
       </Canvas>
     </>
-  )
-};
+  );
+}
 
 export default KittyHeader;
