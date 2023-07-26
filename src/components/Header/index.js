3c154b01ae95a1e234e05397210543b927b8d22d
sjfox <sjfcodes@gmail.com>
2023-07-26 08:36:51 -0700

    delete file drop component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,33 +1,35 @@
-import React, { useState } from 'react';
-import { Redirect } from "react-router-dom"
-import { Container, Row, Col, TextInput, Button, Icon } from 'react-materialize';
-import UserChip from '../UserChip';
+import React from "react";
+import { Row, Col } from "react-materialize";
+import UserChip from "../UserChip";
 import Logout from "../Logout";
-import "./style.css"
+import "./style.css";
 
-function Header({ audio, userData, setUserData, setIsPlaying }) {
-
-  const [redirect, setRedirect] = useState()
-
+function Header({ userData, setUserData, setIsPlaying }) {
   const handleHome = () => {
-    setIsPlaying(false)
-    window.location.href = "/"
-  }
+    setIsPlaying(false);
+    window.location.href = "/";
+  };
 
   return (
     <div className="header">
-      { userData.isLoggedIn
-        ? <Row>
+      {userData.isLoggedIn ? (
+        <Row>
           <Col s={1}>
-            <a onClick={handleHome} style={{ cursor: "pointer" }}><h6 >Radcats Karaoke</h6></a>
+            {/* TODO: use router element for redirect */}
+            <a onClick={handleHome} style={{ cursor: "pointer" }}>
+              <h6>Radcats Karaoke</h6>
+            </a>
           </Col>
           <Col className="right">
-            <Logout userData={userData} setUserData={setUserData} setIsPlaying={setIsPlaying} />
+            <Logout
+              userData={userData}
+              setUserData={setUserData}
+              setIsPlaying={setIsPlaying}
+            />
             <UserChip userData={userData} />
           </Col>
         </Row>
-        : null
-      }
+      ) : null}
     </div>
   );
 }
